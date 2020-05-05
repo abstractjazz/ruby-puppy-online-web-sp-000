884429ff58cf1049126d3eb9c0f632050fcf1324
Evan Burton <evan@heyevan.com>
2020-05-05 04:29:49 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+
+@@all = [ ]
+
+def save
+  @@all << self
+end
+
+def initialize(name)
+@name = name
+save
+end
+
+def self.all
+@@all
+end
+
+self.clear_all
+@@all.clear  
+end
+
+def self.print_all
+@@all.each do |dogs|
+  puts dogs
+    end
+  end
+end
